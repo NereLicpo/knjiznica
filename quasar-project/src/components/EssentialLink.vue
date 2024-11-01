dadf67aaa822396bf054b816ed86fad8bf412138
NereLicpo <rpolic@veleri.hr>
2024-11-01 23:34:00 +0100

    quasar projekt

---
+++ quasar-project/src/components/EssentialLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-item @click="navigate">
+    <q-item-section avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import { useRouter } from 'vue-router'
+
+const props = defineProps(['title', 'caption', 'icon', 'link'])
+const router = useRouter()
+
+function navigate() {
+  router.push(link)
+}
+</script>
+
+<style scoped>
+/* Stilovi za EssentialLink */
+</style>
---
+++ quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,114 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+        <q-toolbar-title class="text-h3">
+          Knjižnica
+        </q-toolbar-title>
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label header>
+          Essential Links
+        </q-item-label>
+
+        <q-item
+          v-for="link in linksList"
+          :key="link.title"
+          @click="navigateTo(link.link)"
+        >
+          <q-item-section avatar>
+            <q-icon :name="link.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ link.title }}</q-item-label>
+            <q-item-label caption>{{ link.caption }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const leftDrawerOpen = ref(false)
+const linksList = [
+  {
+    title: 'Naslovnica',
+    caption: 'Početna stranica',
+    icon: 'home',
+    link: '/'
+  },
+  {
+    title: 'Popis knjiga',
+    caption: 'Popis svih dostupnih knjiga',
+    icon: 'book',
+    link: '/popisKnjiga'
+  },
+  {
+    title: 'Pretraživanje',
+    caption: 'Pretražujte knjige',
+    icon: 'search',
+    link: '/pretrazivanje'
+  },
+  {
+    title: 'O nama',
+    caption: 'Informacije o knjižnici',
+    icon: 'info',
+    link: '/o_nama'
+  },
+  {
+    title: 'Lokacija',
+    caption: 'Gdje se nalazimo',
+    icon: 'place',
+    link: '/lokacija'
+  },
+  {
+    title: 'Login',
+    caption: 'Prijava',
+    icon: 'login',
+    link: '/login'
+  },
+  {
+    title: 'Registracija',
+    caption: 'Kreirajte račun',
+    icon: 'person_add',
+    link: '/registracija'
+  }
+]
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+
+function navigateTo(link) {
+  router.push(link)
+}
+</script>
+
+<style scoped>
+/* Ovdje možete dodati stilove specifične za ovaj layout */
+</style>
